b8a6cc9f6ce05fd34002945040314d4905249b8e
Caly Carisse <caly.cirilo@gmail.com>
2026-04-18 23:34:05 -0700

    YEAH!

--- backend/ai_profile.py
+++ backend/ai_profile.py
@@ -8,4 +8,19 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 
 # GENERATE THREATS YEAHHHH!
 def generate_profile(photo_path, authorized, name="Unknown"):  # if the face recognizer returns "Unknown"
-    
+    # load pic
+    img = PIL.Image.open(photo_path)
+
+    # initialize statuses
+    status = f"AUTHORIZED AGENT: {name}" if authorized
+    else "UNAUTHORIZED INTRUDER"
+
+    # prompt to generate threat profile (i just asked chat to make me this prompt)
+    prompt = prompt = f"""You are a spy intelligence system called ARGUS. 
+    This subject is {status}. 
+    Generate a short dramatic threat briefing in 2-3 sentences.
+    Include: physical description, demeanor, threat level (LOW/MEDIUM/HIGH), recommended action.
+    Format it like a classified intelligence report. Be dramatic and cinematic."""
+
+    response = model.generate_content([prompt, img])
+    return response.text
