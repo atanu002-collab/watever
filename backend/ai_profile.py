b8c332828bd0f219f24e1bfdc4e4fee96ee86efc
Caly Carisse <caly.cirilo@gmail.com>
2026-04-18 23:25:18 -0700

    connects api

---
+++ backend/ai_profile.py
@@ -0,0 +1,7 @@
+import google.generativeai as genai
+import os
+import PIL.Image
+
+# calls api key
+genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
+model = genai.GenerativeModel('gemini-1.5-flash')
