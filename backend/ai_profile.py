f635ce926314edd85523fe4685686a003dbe77a9
Caly Carisse <caly.cirilo@gmail.com>
2026-04-18 23:27:23 -0700

    yum

--- backend/ai_profile.py
+++ backend/ai_profile.py
@@ -5,3 +5,7 @@ import PIL.Image
 # calls api key
 genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
 model = genai.GenerativeModel('gemini-1.5-flash')
+
+# GENERATE THREATS YEAHHHH!
+def generate_profile(photo_path, authorized, name="Unknown"):  # if the face recognizer returns "Unknown"
+    
