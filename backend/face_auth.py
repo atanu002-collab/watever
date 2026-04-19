8759ef4af2bfccca4f03b59e52add2d89e842260
Caly Carisse <caly.cirilo@gmail.com>
2026-04-18 22:33:55 -0700

    added face classifier

--- backend/face_auth.py
+++ backend/face_auth.py
@@ -5,3 +5,5 @@ import numpy as np
 # learns faces by analyzing patterns (aka light n dark pixels)
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 
+# identify which part of imgs are faces 
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
