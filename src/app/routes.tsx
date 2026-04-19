6af4c7912bc09e98b8ebf335e5a7a09da7271eee
Yuvika Shiv <yuvikashiv03@gmail.com>
2026-04-18 21:08:17 -0700

    added proper routes

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -3,6 +3,9 @@ import { Dashboard } from './pages/dashboard'
 import { LiveFeed } from './pages/liveFeed'
 import { Security } from './pages/security'
 import { Alerts } from './pages/alerts'
+import { Auth } from './pages/auth'
+import { Unauthorized } from './pages/unauthorized'
+
 
 export const router = createBrowserRouter([
   {
@@ -21,4 +24,12 @@ export const router = createBrowserRouter([
     path: '/alerts',
     Component: Alerts,
   },
+  {
+    path: '/auth',
+    Component: Auth,
+  },
+  {
+    path: '/unauthorized',
+    Component: Unauthorized,
+  }
 ])
