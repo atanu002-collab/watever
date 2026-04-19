b4cf6c68a2017a7bb7f8d5ff4998b66703f9739b
Caly Carisse <caly.cirilo@gmail.com>
2026-04-18 20:46:20 -0700

    finished finding port function

--- backend/arduino_listen.py
+++ backend/arduino_listen.py
@@ -3,4 +3,9 @@ import serial.tools.list_ports #scans + lists all connected ports
 from camera import capture_photo #imports the capture_photo func
 
 def find_arduino_port():
-    
+    ports = serial.tools.list_ports.comports()
+
+    for port in ports: # looks for the buzzwords in port description
+        if 'Arduino' in port.description or 'CH340' in port.description or 'USB' in port.description:
+            return port.device
+        return None  # nothing is found
